c6e28c657d8ecfc012321a4f5c092b7fbf977476
Tero @RC-Thoughts.com <tero@rc-thoughts.com>
2018-01-15 18:07:45 +0200

    v.1.5 - Update

--- Source/RCT-Empt.lua
+++ Source/RCT-Empt.lua
@@ -6,28 +6,25 @@
 	Settings: Select sensor, working time, alarm-threshold 
 	and audio-file to be played if battery is empty.
 	
-	Localisation-file has to be as /Apps/Lang/RCT-Empt.jsn
+	Localization-file has to be as /Apps/Lang/RCT-Empt.jsn
 	
 	Spanish translation courtesy from César Casado
-	Italian translation courtesy from Fabrizio Zaini 
+	Italian translation courtesy from Fabrizio Zaini
 	---------------------------------------------------------
 	Empty Battery Alarm is a part of RC-Thoughts Jeti Tools.
 	---------------------------------------------------------
-	Released under MIT-license by Tero @ RC-Thoughts.com 2016
+                Released under MIT-license
+     Copyright Tero Salminen and RC-Thoughts.com 2016 - 2018
 	---------------------------------------------------------
 --]]
 collectgarbage()
 ----------------------------------------------------------------------
 -- Locals for the application
-local senso1, senso2, senso3, time1, time2, time3 
-local senid1, senid2, senid3, alarm1, alarm2, alarm3
-local sid1, sparam1, sid2, sparam2, sid3, sparam3
-local senpa1, senpa2, senpa3, vFile1, vFile2, vfile3
-local repeat1, repeat2, repeat3
+local time1, time2, time3, alarm1, alarm2, alarm3
+local senid1, senid2, senid3, sparam1, sparam2, sparam3
+local vFile1, vFile2, vfile3, repeat1, repeat2, repeat3
 local vFile1Played, vFile2Played, vFile3Played
-local sensoLalist1 = {"..."}
-local sensoIdlist1 = {"..."}
-local sensoPalist1 = {"..."}
+local sensorsAvailable = {"..."}
 local repeatlist = {}
 ----------------------------------------------------------------------
 -- Read translations
@@ -39,67 +36,42 @@ local function setLanguage()
         trans3 = obj[lng] or obj[obj.default]
     end
 end
---------------------------------------------------------------------------------
--- Read available sensors for user to select
-local sensors = system.getSensors()
-for i,sensor in ipairs(sensors) do
-	if (sensor.label ~= "") then
-		table.insert(sensoLalist1, string.format("%s", sensor.label))
-		table.insert(sensoIdlist1, string.format("%s", sensor.id))
-		table.insert(sensoPalist1, string.format("%s", sensor.param))
-	end
-end
 ----------------------------------------------------------------------
 -- Store settings when changed by user
 local function sensor1Changed(value)
-	senso1=value
-	senid1=value
-	senpa1=value
-	system.pSave("senso1",value)
-	system.pSave("senid1",value)
-	system.pSave("senpa1",value)
-	sid1 = string.format("%s", sensoIdlist1[senid1])
-	sparam1 = string.format("%s", sensoPalist1[senpa1])
-	if (sid1 == "...") then
-		sid1 = 0
-		sparam1 = 0
-	end
-	system.pSave("sid1", sid1)
+    senid1  = sensorsAvailable[value].id
+	sparam1  = sensorsAvailable[value].param
+	system.pSave("senid1", senid1)
 	system.pSave("sparam1", sparam1)
 end
 
 local function sensor2Changed(value)
-	senso2=value
-	senid2=value
-	senpa2=value
-	system.pSave("senso2",value)
-	system.pSave("senid2",value)
-	system.pSave("senpa2",value)
-	sid2 = string.format("%s", sensoIdlist1[senid2])
-	sparam2 = string.format("%s", sensoPalist1[senpa2])
-	if (sid2 == "...") then
-		sid2 = 0
-		sparam2 = 0
-	end
-	system.pSave("sid2", sid2)
+	senid2  = sensorsAvailable[value].id
+	sparam2  = sensorsAvailable[value].param
+	system.pSave("senid2", senid2)
 	system.pSave("sparam2", sparam2)
 end
 
 local function sensor3Changed(value)
-	senso3=value
-	senid3=value
-	senpa3=value
-	system.pSave("senso3",value)
-	system.pSave("senid3",value)
-	system.pSave("senpa3",value)
-	sid3 = string.format("%s", sensoIdlist1[senid3])
-	sparam3 = string.format("%s", sensoPalist1[senpa3])
-	if (sid3 == "...") then
-		sid3 = 0
-		sparam3 = 0
-	end
-	system.pSave("sid3", sid3)
+	senid3  = sensorsAvailable[value].id
+	sparam3  = sensorsAvailable[value].param
+	system.pSave("senid3", senid3)
 	system.pSave("sparam3", sparam3)
+end
+
+local function batte1nameChanged(value)
+    batte1name=value
+    system.pSave("batte1name",value)
+end
+
+local function batte2nameChanged(value)
+    batte2name=value
+    system.pSave("batte2name",value)
+end
+
+local function batte3nameChanged(value)
+    batte3name=value
+    system.pSave("batte3name",value)
 end
 
 local function time1Changed(value)
@@ -162,18 +134,47 @@ local function repeat3Changed(value)
 	system.pSave("repeat3",value)
 end
 ----------------------------------------------------------------------
--- Draw the main form (Application inteface)
+-- Draw the main form (Application interface)
 local function initForm()
+    -- List sensors only if menu is active to preserve memory at runtime 
+    -- (measured up to 25% save if menu is not opened)
+    sensorsAvailable = {}
+    local sensors = system.getSensors();
+    local list={}
+    local curIndex1, curIndex2, curIndex3 = -1, -1, .1
+    local descr = ""
+    for index,sensor in ipairs(sensors) do 
+        if(sensor.param == 0) then
+            descr = sensor.label
+            else
+            list[#list + 1] = string.format("%s - %s", descr, sensor.label)
+            sensorsAvailable[#sensorsAvailable + 1] = sensor
+            if(sensor.id == senid1 and sensor.param == sparam1) then
+                curIndex1 =# sensorsAvailable
+            end
+            if(sensor.id == senid2 and sensor.param == sparam2) then
+                curIndex2 =# sensorsAvailable
+            end
+            if(sensor.id == senid3 and sensor.param == sparam3) then
+                curIndex3 =# sensorsAvailable
+            end
+        end
+    end 
+    
 	form.addRow(1)
 	form.addLabel({label="---     RC-Thoughts Jeti Tools      ---",font=FONT_BIG})
 	
 	-- Battery 1
 	form.addRow(1)
 	form.addLabel({label=trans3.batte1,font=FONT_BOLD})
+    
+    form.addRow(2)
+    form.addLabel({label=trans3.batteryName,width=140})
+    form.addTextbox(batte1name,18,batte1nameChanged,{width=167})
 	
 	form.addRow(2)
 	form.addLabel({label=trans3.sensorSelect})
-	form.addSelectbox(sensoLalist1,senso1,true,sensor1Changed)
+	form.addSelectbox(list,curIndex1,true,sensor1Changed)
 	
 	form.addRow(2)
 	form.addLabel({label=trans3.selTime})
@@ -194,10 +195,14 @@ local function initForm()
 	-- Battery 2
 	form.addRow(1)
 	form.addLabel({label=trans3.batte2,font=FONT_BOLD})
+    
+    form.addRow(2)
+    form.addLabel({label=trans3.batteryName,width=140})
+    form.addTextbox(batte2name,18,batte2nameChanged,{width=167})
 	
 	form.addRow(2)
 	form.addLabel({label=trans3.sensorSelect})
-	form.addSelectbox(sensoLalist1,senso2,true,sensor2Changed)
+	form.addSelectbox(list,curIndex2,true,sensor2Changed)
 	
 	form.addRow(2)
 	form.addLabel({label=trans3.selTime})
@@ -218,10 +223,14 @@ local function initForm()
 	-- Battery 3
 	form.addRow(1)
 	form.addLabel({label=trans3.batte3,font=FONT_BOLD})
+    
+    form.addRow(2)
+    form.addLabel({label=trans3.batteryName,width=140})
+    form.addTextbox(batte3name,18,batte3nameChanged,{width=167})
 	
 	form.addRow(2)
 	form.addLabel({label=trans3.sensorSelect})
-	form.addSelectbox(sensoLalist1,senso3,true,sensor3Changed)
+	form.addSelectbox(list,curIndex3,true,sensor3Changed)
 	
 	form.addRow(2)
 	form.addLabel({label=trans3.selTime})
@@ -247,7 +256,7 @@ end
 -- Runtime functions
 local function loop()
 	local tCurrent = system.getTime()
-	local sense1 = system.getSensorByID(sid1, sparam1)
+	local sense1 = system.getSensorByID(senid1, sparam1)
 	if(sense1 and sense1.valid) then
 		if(tInit1 and tInit1 > 1) then
 			else
@@ -260,13 +269,13 @@ local function loop()
 			if(sense1.value <= (alarm1 / 10)) then
 				if(tCurrent >= tPlay1 and vFile1played ~= 1) then
 					if (repeat1 == 2) then
-						system.messageBox(trans3.sysWarn1,5)
+                        system.messageBox(trans3.sysWarn .. batte1name,5)
 						system.playFile(vFile1,AUDIO_AUDIO_QUEUE)
 						system.playFile(vFile1,AUDIO_AUDIO_QUEUE)
 						system.playFile(vFile1,AUDIO_AUDIO_QUEUE)
 						vFile1played = 1
 						else
-						system.messageBox(trans3.sysWarn1,5)
+						system.messageBox(trans3.sysWarn .. batte1name,5)
 						system.playFile(vFile1,AUDIO_AUDIO_QUEUE)
 						vFile1played = 1
 					end
@@ -280,7 +289,7 @@ local function loop()
 		tInit1 = 0
 	end
 	--- Battery 2
-	local sense2 = system.getSensorByID(sid2, sparam2)
+	local sense2 = system.getSensorByID(senid2, sparam2)
 	if(sense2 and sense2.valid) then
 		if(tInit2 and tInit2 > 1) then
 			else
@@ -293,13 +302,13 @@ local function loop()
 			if(sense2.value <= (alarm2 / 10)) then
 				if(tCurrent >= tPlay2 and vFile2played ~= 1) then
 					if (repeat2 == 2) then
-						system.messageBox(trans3.sysWarn2,5)
+                        system.messageBox(trans3.sysWarn .. batte2name,5)
 						system.playFile(vFile2,AUDIO_AUDIO_QUEUE)
 						system.playFile(vFile2,AUDIO_AUDIO_QUEUE)
 						system.playFile(vFile2,AUDIO_AUDIO_QUEUE)
 						vFile2played = 1
 						else
-						system.messageBox(trans3.sysWarn2,5)
+                        system.messageBox(trans3.sysWarn .. batte2name,5)
 						system.playFile(vFile2,AUDIO_AUDIO_QUEUE)
 						vFile2played = 1
 					end
@@ -313,7 +322,7 @@ local function loop()
 		tInit2 = 0
 	end
 	--- Battery 3
-	local sense3 = system.getSensorByID(sid3, sparam3)
+	local sense3 = system.getSensorByID(senid3, sparam3)
 	if(sense3 and sense3.valid) then
 		if(tInit3 and tInit3 > 1) then
 			else
@@ -326,13 +335,13 @@ local function loop()
 			if(sense3.value <= (alarm3 / 10)) then
 				if(tCurrent >= tPlay3 and vFile3played ~= 1) then
 					if (repeat3 == 2) then
-						system.messageBox(trans3.sysWarn3,5)
+						system.messageBox(trans3.sysWarn .. batte3name,5)
 						system.playFile(vFile3,AUDIO_AUDIO_QUEUE)
 						system.playFile(vFile3,AUDIO_AUDIO_QUEUE)
 						system.playFile(vFile3,AUDIO_AUDIO_QUEUE)
 						vFile3played = 1
 						else
-						system.messageBox(trans3.sysWarn3,5)
+                        system.messageBox(trans3.sysWarn .. batte3name,5)
 						system.playFile(vFile3,AUDIO_AUDIO_QUEUE)
 						vFile3played = 1
 					end
@@ -351,20 +360,11 @@ end
 -- Application initialization
 local function init()
 	system.registerForm(1,MENU_APPS,trans3.appName,initForm)
-	senso1 = system.pLoad("senso1",0)
 	senid1 = system.pLoad("senid1",0)
-	senpa1 = system.pLoad("senpa1",0)
-	sid1 = system.pLoad("sid1",0)
 	sparam1 = system.pLoad("sparam1",0)
-	senso2 = system.pLoad("senso2",0)
 	senid2 = system.pLoad("senid2",0)
-	senpa2 = system.pLoad("senpa2",0)
-	sid2 = system.pLoad("sid2",0)
 	sparam2 = system.pLoad("sparam2",0)
-	senso3 = system.pLoad("senso3",0)
 	senid3 = system.pLoad("senid3",0)
-	senpa3 = system.pLoad("senpa3",0)
-	sid3 = system.pLoad("sid3",0)
 	sparam3 = system.pLoad("sparam3",0)
 	time1 = system.pLoad("time1",0)
 	time2 = system.pLoad("time2",0)
@@ -378,12 +378,15 @@ local function init()
 	repeat1 = system.pLoad("repeat1",1)
 	repeat2 = system.pLoad("repeat2",1)
 	repeat3 = system.pLoad("repeat3",1)
+    batte1name = system.pLoad("batte1name",trans3.batte1name)
+    batte2name = system.pLoad("batte2name",trans3.batte2name)
+    batte3name = system.pLoad("batte3name",trans3.batte3name)
 	table.insert(repeatlist,trans3.neg)
 	table.insert(repeatlist,trans3.pos)
     collectgarbage()
 end
 ----------------------------------------------------------------------
-emptVersion = "1.4"
+emptVersion = "1.5"
 setLanguage()
 collectgarbage()
 return {init=init, loop=loop, author="RC-Thoughts", version=emptVersion, name=trans3.appName}
